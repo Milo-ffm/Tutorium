5d825510dc84bab6b4784eb34290c3663d09aa37
Milo-ffm <131757806+Milo-ffm@users.noreply.github.com>
2023-04-27 15:34:18 +0000

    [save]Tutorial 1 Notes 1

---
+++ Lecture 1/Helll_world.R
@@ -0,0 +1,70 @@
+# import packages
+# data.table
+# ggplot2
+#dpplr
+#knitr
+#magrittr
+
+library(data.table)
+library(ggplot2)
+library(dplyr)
+library(knitr)
+library(magrittr)
+
+print("Hello World!")
+
+# generate 100 random numbers for x and y
+# plot with basic R plot function
+
+x <- rnorm(100)
+y <- rnorm(100)
+plot(x,y)
+
+use data.table create a 3 row and 2 column data table
+my_data <- data.table(x = 1:3,y = 4:6)
+
+# create a data.table with 3 columns and 10 rows
+# one column is a sequence of numbers
+# one column is hello repeated 10 times
+# third column is drawn from normal distribution
+
+my_data <- data.table(x = 1:10,y = rep("hello",10),z = rnorm(10))
+
+str(my_data)
+head(my_data)
+
+    # create a vector with 5 elements all "female"
+
+    xfm <- rep("female")
+
+    xml <- rep("male", 5)
+
+# create another one filled with "male"
+
+gender <- c(xfm, xml)
+
+# add gender to my_data
+
+my_data$gender <- gender
+
+str(my_data)
+
+# select y column
+
+my_data$y
+
+# filter out z > 0
+my_data %>% 
+    .[z > 0] %>%
+    # filter out gender female
+    .[gender == "female"] %>%
+    kable()
+
+#select z and gender columns
+
+my_data %>%
+    .[, .(z, gender)] %>%
+    .[z > 0, ] %>%
+    kable()
+
+install.packages("curl")
